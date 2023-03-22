9458d341b25729bd93712f4109bdee4090616d55
MohammedYasserr <bmbmyasser@gmail.com>
2023-03-23 01:07:10 +0200

    setting up the APP class that will expose the REST api

--- src/main/java/com/natWest/app/App.java
+++ src/main/java/com/natWest/app/App.java
@@ -1,13 +1,33 @@
 package com.natWest.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.natWest.app.config.Constant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
+
+@SpringBootApplication
+public class App {
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(App.class);
+        SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
+        defaultToDevProfile(application, source);
+
+        Environment env = application.run(args).getEnvironment();
+        log.info("Running at Port {}", env.getProperty("server.port"));
+    }
+
+    /**
+     * If no profile has been configured, set by default the "dev" profile.
+     */
+    private static void defaultToDevProfile(SpringApplication app, SimpleCommandLinePropertySource source) {
+        if (!source.containsProperty("spring.profiles.active") &&
+                !System.getenv().containsKey("SPRING_PROFILES_ACTIVE")) {
+            app.setAdditionalProfiles(Constant.SPRING_PROFILE_DEVELOPMENT);
+        }
     }
 }
